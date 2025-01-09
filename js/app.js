7c9a37704a80e8c99d8bc49ab18ecad8b33e0128
Maya Sides <maya.sides@gmail.com>
2025-01-09 11:30:39 -0500

    repo

---
+++ js/app.js
@@ -0,0 +1,10 @@
+objectFitVideos();
+
+// Select the video element by its ID
+const video = document.getElementById('myVideo');
+
+// Enable autoplay
+video.autoplay = true;
+
+// Load the video to apply autoplay
+video.load();
