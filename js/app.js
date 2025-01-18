19db534eac989d272b1a190899112a2ce7143545
Maya Sides <maya.sides@gmail.com>
2025-01-18 13:45:35 -0600

    fixing infinite moving txt

--- js/app.js
+++ js/app.js
@@ -8,3 +8,37 @@ video.autoplay = true;
 
 // Load the video to apply autoplay
 video.load();
+
+let currentScroll = 0;
+let isScrollingDown = true;
+let arrows = document.querySelectorAll(".arrow");
+
+let tween = gsap.to(".marquee__part", {
+    xPercent: -100,
+    repeat: -1,
+    duration: 5,
+    ease: "linear"
+}).totalProgess(0.5);
+
+gsap.set(".marquee__inner", {xPercent: -50});
+
+window.addEventListener("scroll", function() {
+   if(window.pageYOffset > currentScroll) {
+    isScrollinDown = true;
+   } else {
+    isScrollinDown = false;
+   }
+
+   gsap.to(tween, {
+    timeScale: isScrollingDown ? 1 : -1,
+   });
+
+   arrows.forEach((arrow) => {
+    if(isScrollingDown) {
+        arrow.classList.remove("active");
+    } else {
+        arrow.classList.add("active");
+    }
+   });
+   currentScroll = window.pageYOffset;
+});
